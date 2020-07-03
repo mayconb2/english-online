ca3ae772a76dbabc5d73d055c93c8b3a4767bcf8
Lucas Krämer de Sousa <lucas.kramer.sousa@gmail.com>
2020-07-02 23:17:13 -0300

    :wrench: - Configuring Firebase

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,22 @@
-const routes = require('express').Router();
+const routes = require('express').Router()
+var admin = require("firebase-admin");
+var serviceAccount = require('../serviceAccountKey.json');
 
-routes.get('/', (req, res) => {
-  res.status(200).json({ message: 'Connected!' });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://my-english-online.firebaseio.com"
 });
 
-module.exports = routes;
+let db = admin.database();
+let userRef = db.ref("users");
+
+routes.get('/', (req, res) => {
+    res.status(200).json({ message: 'Connected!' })
+})
+
+routes.post('/add-something', (req, res) => {
+    userRef.child('teste').set(req.body.xablau)
+    res.status(200).json({ message: 'XABLAU' })
+})
+
+module.exports = routes
